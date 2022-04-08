8d2d91af3fbc4ce8928dde426611a1321ecbb770
Lennart Dreves <lennart@lennmedia.nl>
2022-04-08 12:42:38 +0200

    Initial version

---
+++ lang/english.php
@@ -0,0 +1,16 @@
+<?php
+$_ADDONLANG['CrispApiError'] = "Crisp API Error";
+$_ADDONLANG['CrispPluginInstalled'] = "Crisp Plugin Succesfully Installed";
+$_ADDONLANG['CrispPluginInstalledMsg'] = "New clients and user profile updates will automatically be synced to Crisp upon registration or update.";
+$_ADDONLANG['CrispPluginNotInstalled'] = "Crisp Plugin Not Installed";
+$_ADDONLANG['CrispPluginNotInstalledMsg'] = "The Crisp Plugin is not installed and the addon is working with limited functionality.<br />Please install the Crisp Plugin by clicking <a href=\"https://app.crisp.chat/initiate/plugin/43613682-40a4-43aa-85a7-3013dbe5f0ad/\">here</a> to get all functionality from this addon.<br />If the Crisp Plugin is installed and you still receive this error, please check the Website ID and License Key in the <a href=\"configaddonmods.php\">Addon Configuration</a>.";
+
+$_ADDONLANG['SyncInfo'] = "Click the button below to (re)sync all existing clients and users in WHMCS to Crisp. The synchronization might take a few hours on large installations and runs in the background.<br />A synchronization is usually only needed once. The plugin will automatically create new clients and process user profile updates.";
+$_ADDONLANG['SyncToCrisp'] = "Sync Clients and Users to Crisp";
+$_ADDONLANG['UpdateWHMCSURLInfo'] = "Click the button below to set or update your WHMCS URL in Crisp.<br />This is only needed once or when you change your WHMCS Admin URL.";
+$_ADDONLANG['UpdateWHMCSURL'] = "Set/Update WHMCS URL";
+
+$_ADDONLANG['SyncConfirmationMsg'] = "Added all clients and users to the synchronization database to be synchronized with Crisp.<br />The synchronization might take a few hours on large installations and will run in the background.";
+$_ADDONLANG['AutoUpdateNotificationMsg'] = "<b>NOTE: Autoupdate of CRM data is <u>disabled</u> in the <a href=\"configaddonmods.php\">Addon Settings</a>. Synchronization will NOT take place till this feature is enabled.</b>";
+$_ADDONLANG['UpdatedWHMCSURLMsg'] = "Updated WHMCS URL in Crisp.";
+$_ADDONLANG['GoBack'] = "Go Back";
