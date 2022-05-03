ff50464a091133c3a928ceb2ed0a2b953bc05f47
Lennart <lennart@lennmedia.nl>
2022-05-03 09:43:02 +0200

    Create dutch.php
    Added Dutch language strings.

---
+++ lang/dutch.php
@@ -0,0 +1,16 @@
+<?php
+$_ADDONLANG['CrispApiError'] = "Crisp API Fout";
+$_ADDONLANG['CrispPluginInstalled'] = "Crisp Plugin succesvol geïnstalleerd";
+$_ADDONLANG['CrispPluginInstalledMsg'] = "Nieuwe klanten en updates van gebruikersprofielen worden automatisch naar Crisp gesynchroniseerd bij registratie of update.";
+$_ADDONLANG['CrispPluginNotInstalled'] = "Crisp Plugin niet geïnstalleerd";
+$_ADDONLANG['CrispPluginNotInstalledMsg'] = "De Crisp Plugin is niet geïnstalleerd en de add-on werkt in gelimiteerde functionaliteit.<br />Installeer de Crisp Plugin door <a href=\"https://app.crisp.chat/initiate/plugin/43613682-40a4-43aa-85a7-3013dbe5f0ad/\">hier</a> te klikken om alle functionaliteit van deze addon te verkrijgen.<br />Als de Crisp Plugin geïnstalleerd is en u ontvangt nog steeds deze melding, controleer dan de website ID en licentiesleutel in de <a href=\"configaddonmods.php\">Add-on configuratie</a>.";
+
+$_ADDONLANG['SyncInfo'] = "Klik op onderstaande knop om alle bestaande klanten en gebruikers in WHMCS te (her)synchroniseren naar Crisp. De synchronisatie kan enkele uren duren op grote installaties en draait op de achtergrond.<br />Een synchronisatie is normaal gesproken maar eenmaal noodzakelijk. De plugin maakt automatisch nieuwe klanten aan en houdt gebruikersprofielen gesynchroniseerd.";
+$_ADDONLANG['SyncToCrisp'] = "Syncroniseer klanten en gebruikers naar Crisp";
+$_ADDONLANG['UpdateWHMCSURLInfo'] = "Klik op onderstaande knop om de WHMCS URL in te stellen of bij te werken in Crisp.<br />Dit is eenmaal noodzakelijk, of in het geval de WHMCS Admin URL wordt aangepast.";
+$_ADDONLANG['UpdateWHMCSURL'] = "Werk WHMCS URL bij";
+
+$_ADDONLANG['SyncConfirmationMsg'] = "Alle klanten en gebruikers toegevoegd zijn toegevoegd aan de wachtrij om gesynchroniseerd te worden naar Crisp.<br />De synchronisatie kan enkele uren duren op grote installaties en draait op de achtergrond.";
+$_ADDONLANG['AutoUpdateNotificationMsg'] = "<b>LET OP: Automatische updates van CRM data staat <u>uitgeschakeld</u> in de <a href=\"configaddonmods.php\">Add-on configuratie</a>. Synchronisatie zal NIET plaatsvinden totdat deze functie wordt ingeschakeld.</b>";
+$_ADDONLANG['UpdatedWHMCSURLMsg'] = "WHMCS URL in Crisp bijgewerkt.";
+$_ADDONLANG['GoBack'] = "Ga terug";
