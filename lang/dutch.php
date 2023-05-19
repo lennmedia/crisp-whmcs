b701ea88bc03236f064403ea0954b55702db4995
Lennart <lennart@lennmedia.nl>
2023-05-19 17:37:53 +0200

    Update locales for v2.12

--- lang/dutch.php
+++ lang/dutch.php
@@ -5,12 +5,22 @@ $_ADDONLANG['CrispPluginInstalledMsg'] = "Nieuwe klanten en updates van gebruike
 $_ADDONLANG['CrispPluginNotInstalled'] = "Crisp Plugin niet geïnstalleerd";
 $_ADDONLANG['CrispPluginNotInstalledMsg'] = "De Crisp Plugin is niet geïnstalleerd en de add-on werkt in gelimiteerde functionaliteit.<br />Installeer de Crisp Plugin door <a href=\"https://app.crisp.chat/initiate/plugin/43613682-40a4-43aa-85a7-3013dbe5f0ad/\">hier</a> te klikken om alle functionaliteit van deze addon te verkrijgen.<br />Als de Crisp Plugin geïnstalleerd is en u ontvangt nog steeds deze melding, controleer dan de website ID en licentiesleutel in de <a href=\"configaddonmods.php\">Add-on configuratie</a>.";
 
+$_ADDONLANG['SyncHeading'] = "Synchroniseer Crisp CRM";
 $_ADDONLANG['SyncInfo'] = "Klik op onderstaande knop om alle bestaande klanten en gebruikers in WHMCS te (her)synchroniseren naar Crisp. De synchronisatie kan enkele uren duren op grote installaties en draait op de achtergrond.<br />Een synchronisatie is normaal gesproken maar eenmaal noodzakelijk. De plugin maakt automatisch nieuwe klanten aan en houdt gebruikersprofielen gesynchroniseerd.";
 $_ADDONLANG['SyncToCrisp'] = "Syncroniseer klanten en gebruikers naar Crisp";
-$_ADDONLANG['UpdateWHMCSURLInfo'] = "Klik op onderstaande knop om de WHMCS URL in te stellen of bij te werken in Crisp.<br />Dit is eenmaal noodzakelijk, of in het geval de WHMCS Admin URL wordt aangepast.";
-$_ADDONLANG['UpdateWHMCSURL'] = "Werk WHMCS URL bij";
+
+$_ADDONLANG['WidgetHeading'] = "Crisp Widget instellingen";
+$_ADDONLANG['WidgetInfo'] = "Selecteer welke opties je wilt tonen in de Crisp Widget.<br />Dit hoeft maar een keer of wanneer je WHMCS Admin URL of widget voorkeuren wijzigen.";
+$_ADDONLANG['ViewAccount'] = "View account";
+$_ADDONLANG['ViewProducts'] = "View products";
+$_ADDONLANG['ViewTickets'] = "View tickets";
+$_ADDONLANG['OpenNewTicket'] = "Open new ticket";
+$_ADDONLANG['LoginAsClient'] = "Login as client";
+$_ADDONLANG['UpdateCrispWidget'] = "Werk Crisp Widget instellingen bij";
 
 $_ADDONLANG['SyncConfirmationMsg'] = "Alle klanten en gebruikers toegevoegd zijn toegevoegd aan de wachtrij om gesynchroniseerd te worden naar Crisp.<br />De synchronisatie kan enkele uren duren op grote installaties en draait op de achtergrond.";
-$_ADDONLANG['AutoUpdateNotificationMsg'] = "<b>LET OP: Automatische updates van CRM data staat <u>uitgeschakeld</u> in de <a href=\"configaddonmods.php\">Add-on configuratie</a>. Synchronisatie zal NIET plaatsvinden totdat deze functie wordt ingeschakeld.</b>";
-$_ADDONLANG['UpdatedWHMCSURLMsg'] = "WHMCS URL in Crisp bijgewerkt.";
+$_ADDONLANG['AutoUpdateNotificationMsg'] = "Automatische updates van CRM data staat <u>uitgeschakeld</u> in de <a href=\"configaddonmods.php\">Add-on configuratie</a>. Synchronisatie zal NIET plaatsvinden totdat deze functie wordt ingeschakeld.";
+$_ADDONLANG['WidgetUpdateConfirmationMsg'] = "Crisp Widget instellingen bijgewerkt. Herlaad Crisp om de veranderingen te zien.";
+
 $_ADDONLANG['GoBack'] = "Ga terug";
+$_ADDONLANG['Redirecting'] = "Een ogenblik alstublieft...";
--- lang/english.php
+++ lang/english.php
@@ -5,12 +5,22 @@ $_ADDONLANG['CrispPluginInstalledMsg'] = "New clients and user profile updates w
 $_ADDONLANG['CrispPluginNotInstalled'] = "Crisp Plugin Not Installed";
 $_ADDONLANG['CrispPluginNotInstalledMsg'] = "The Crisp Plugin is not installed and the addon is working with limited functionality.<br />Please install the Crisp Plugin by clicking <a href=\"https://app.crisp.chat/initiate/plugin/43613682-40a4-43aa-85a7-3013dbe5f0ad/\">here</a> to get all functionality from this addon.<br />If the Crisp Plugin is installed and you still receive this error, please check the Website ID and License Key in the <a href=\"configaddonmods.php\">Addon Configuration</a>.";
 
+$_ADDONLANG['SyncHeading'] = "Sync Crisp CRM";
 $_ADDONLANG['SyncInfo'] = "Click the button below to (re)sync all existing clients and users in WHMCS to Crisp. The synchronization might take a few hours on large installations and runs in the background.<br />A synchronization is usually only needed once. The plugin will automatically create new clients and process user profile updates.";
 $_ADDONLANG['SyncToCrisp'] = "Sync Clients and Users to Crisp";
-$_ADDONLANG['UpdateWHMCSURLInfo'] = "Click the button below to set or update your WHMCS URL in Crisp.<br />This is only needed once or when you change your WHMCS Admin URL.";
-$_ADDONLANG['UpdateWHMCSURL'] = "Set/Update WHMCS URL";
+
+$_ADDONLANG['WidgetHeading'] = "Crisp Widget Settings";
+$_ADDONLANG['WidgetInfo'] = "Please select which options you want to present in the Crisp Widget.<br />This is only needed once or when you change your WHMCS Admin URL or widget preferences.";
+$_ADDONLANG['ViewAccount'] = "View account";
+$_ADDONLANG['ViewProducts'] = "View products";
+$_ADDONLANG['ViewTickets'] = "View tickets";
+$_ADDONLANG['OpenNewTicket'] = "Open new ticket";
+$_ADDONLANG['LoginAsClient'] = "Login as client";
+$_ADDONLANG['UpdateCrispWidget'] = "Update Crisp Widget";
 
 $_ADDONLANG['SyncConfirmationMsg'] = "Added all clients and users to the synchronization database to be synchronized with Crisp.<br />The synchronization might take a few hours on large installations and will run in the background.";
-$_ADDONLANG['AutoUpdateNotificationMsg'] = "<b>NOTE: Auto-update of CRM data is <u>disabled</u> in the <a href=\"configaddonmods.php\">Addon Settings</a>. Synchronization will NOT take place till this feature is enabled.</b>";
-$_ADDONLANG['UpdatedWHMCSURLMsg'] = "Updated WHMCS URL in Crisp.";
+$_ADDONLANG['AutoUpdateNotificationMsg'] = "Auto-update of CRM data is <u>disabled</u> in the <a href=\"configaddonmods.php\">Addon Settings</a>. Synchronization will NOT take place till this feature is enabled.";
+$_ADDONLANG['WidgetUpdateConfirmationMsg'] = "Updated Crisp Widget settings. Please re-load Crisp to see the changes.";
+
 $_ADDONLANG['GoBack'] = "Go Back";
+$_ADDONLANG['Redirecting'] = "One moment please...";
